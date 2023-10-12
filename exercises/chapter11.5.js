c994b60f8792bec20d84bf342b21d9273c94d6e6
André Rodrigues <andre.rodrigues98@outlook.com>
2023-10-11 21:59:34 -0300

    refactor: chapter 11 exercise 5

--- exercises/chapter11.5.js
+++ exercises/chapter11.5.js
@@ -1,37 +1,10 @@
-const shortPath = (columns, rows) => {
-  const matrix = [];
-
-  for (let row = 0; row < rows; row++) {
-    matrix[row] = [];
+const uniquePaths = (rows, columns) => {
+  if (rows === 1 || columns === 1) {
+    return 1;
   }
 
-  const getShortPath = (matrix = [], columns, row = 0) => {
-    if (matrix.length - 1 < row) {
-      return matrix[row - 1][columns - 1];
-    }
-
-    for (let column = 0; column < columns; column++) {
-      if (column === 0 && row === 0) {
-        matrix[row][column] = 0;
-        continue;
-      }
-
-      const abovePosition = row - 1 >= 0 ? row - 1 : 0;
-      const leftPosition = column - 1 >= 0 ? column - 1 : 0;
-
-      const aboveValue = matrix[abovePosition][column] | 0;
-      const leftValue = matrix[row][leftPosition] | 0;
-
-      const result = leftValue <= aboveValue ? aboveValue + 1 : leftValue + 1;
-
-      matrix[row][column] = result;
-    }
-
-    return getShortPath(matrix, columns, row + 1);
-  };
-
-  return getShortPath(matrix, columns);
+  return uniquePaths(rows - 1, columns) + uniquePaths(rows, columns - 1);
 };
 
-console.log(shortPath(5, 4));
-// output: 7
+console.log(uniquePaths(5, 4));
+// output: 35
