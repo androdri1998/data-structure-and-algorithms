6fa0f0745e83d77648ccb4583dd94815a25ec37f
André Rodrigues <andre.rodrigues98@outlook.com>
2023-10-11 21:47:46 -0300

    refactor: remaming column variables

--- exercises/chapter11.5.js
+++ exercises/chapter11.5.js
@@ -1,36 +1,36 @@
-const shortPath = (cols, rows) => {
+const shortPath = (columns, rows) => {
   const matrix = [];
 
   for (let row = 0; row < rows; row++) {
     matrix[row] = [];
   }
 
-  const getShortPath = (matrix = [], cols, row = 0) => {
+  const getShortPath = (matrix = [], columns, row = 0) => {
     if (matrix.length - 1 < row) {
-      return matrix[row - 1][cols - 1];
+      return matrix[row - 1][columns - 1];
     }
 
-    for (let col = 0; col < cols; col++) {
-      if (col === 0 && row === 0) {
-        matrix[row][col] = 0;
+    for (let column = 0; column < columns; column++) {
+      if (column === 0 && row === 0) {
+        matrix[row][column] = 0;
         continue;
       }
 
       const abovePosition = row - 1 >= 0 ? row - 1 : 0;
-      const leftPosition = col - 1 >= 0 ? col - 1 : 0;
+      const leftPosition = column - 1 >= 0 ? column - 1 : 0;
 
-      const aboveValue = matrix[abovePosition][col] | 0;
+      const aboveValue = matrix[abovePosition][column] | 0;
       const leftValue = matrix[row][leftPosition] | 0;
 
       const result = leftValue <= aboveValue ? aboveValue + 1 : leftValue + 1;
 
-      matrix[row][col] = result;
+      matrix[row][column] = result;
     }
 
-    return getShortPath(matrix, cols, row + 1);
+    return getShortPath(matrix, columns, row + 1);
   };
 
-  return getShortPath(matrix, cols);
+  return getShortPath(matrix, columns);
 };
 
 console.log(shortPath(5, 4));
